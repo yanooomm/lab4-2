0680ad886a05f3afd53b4a44d52e7f72011d05a2
yanooomm <115118650+yanooomm@users.noreply.github.com>
2023-02-04 13:51:36 +0500

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -21,7 +21,10 @@ int main()
         f.close();
     }
     else
+    {
         cout << "Ошибка при открытии файла";
+        return 0;
+    }
     for (int i = 0; i < vec.size(); i++)
         cout << vec[i] << ' ';
     int size = vec.size();
